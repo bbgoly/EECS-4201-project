dc1c3194ded52ec8f724fa853c63457da84187e6
SKYORKU <salmankayaniyork@gmail.com>
2025-10-11 21:12:23 -0400

    Fixed module instantiations in pd2.sv

--- project/pd2/design/code/pd2.sv
+++ project/pd2/design/code/pd2.sv
@@ -13,42 +13,48 @@ module pd2 #(
     parameter int DWIDTH = 32)(
     input logic clk,
     input logic reset
-);
+);	
+    // imemory signals
+    logic [DWIDTH - 1:0] addr_i;
+    logic [DWIDTH - 1:0] data_i;
+    logic write_en;
+    logic read_en;
+       
+    // Fetch signals
+    logic [DWIDTH - 1:0] f_pc;
+    logic [DWIDTH - 1:0] f_insn;
+       
+    memory #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASE_ADDR(32'h01000000)
+       ) memory1 (
+        .clk(clk),
+        .rst(reset),
+        .addr_i(f_pc),
+        .data_i(data_i),
+        .read_en_i(read_en),
+        .write_en_i(write_en),
+        .data_o(f_insn)
+   );
+ 
+    assign read_en = 1'b1;
+    assign write_en = 1'b0;
 
+    // Fetch
+    fetch #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASEADDR(32'h01000000)
+    ) fetch1 (
+        .clk(clk),
+        .rst(reset),
+        .pc_o(f_pc),            
+        .insn_o(f_insn)         
+    );
 
-logic [AWIDTH-1:0] addr;
-logic [DWIDTH-1:0] data_in;
-logic read_en;
-logic write_en;
-logic [DWIDTH-1:0] data_out;
-
-memory #(.AWIDTH(AWIDTH), .DWIDTH(DWIDTH)) memory_0 (
-  .clk (clk),
-  .rst (reset),
-  .addr_i (addr),
-  .data_i (data_in),
-  .read_en_i (read_en),
-  .write_en_i (write_en),
-  .data_o (data_out)
-);
-
-
-logic [AWIDTH - 1:0] f_pc;
-logic [DWIDTH - 1:0] f_insn;
-
-fetch #(.AWIDTH(AWIDTH), .DWIDTH(DWIDTH)) fetch_0 (
-  .clk (clk),
-  .rst (reset),
-  .pc_o (f_pc),
-  .insn_o (f_insn)
-);
-
-
-//logic [DWIDTH-1:0] d_imm;
-     //logic [DWIDTH - 1:0] insn_in;
-     //logic [DWIDTH - 1:0] pc_in;
      logic [AWIDTH-1:0] d_pc;
-     logic [DWIDTH-1:0] insn_out;
+     logic [DWIDTH-1:0] d_insn;
      logic [6:0] d_opcode;
      logic [4:0] d_rd;
      logic [4:0] d_rs1;
@@ -58,15 +64,14 @@ fetch #(.AWIDTH(AWIDTH), .DWIDTH(DWIDTH)) fetch_0 (
      logic [4:0] d_shamt;
      logic [DWIDTH-1:0] d_imm;
 
-decode #(.DWIDTH(DWIDTH), .AWIDTH(AWIDTH)) decode_0 (
-
+decode #(.DWIDTH(DWIDTH), .AWIDTH(AWIDTH)) decode1 (
     .clk (clk),
     .rst (reset),
     .insn_i (f_insn),
     .pc_i (f_pc),
 
     .pc_o (d_pc),
-    .insn_o (insn_out),
+    .insn_o (f_insn),
     .opcode_o (d_opcode),
     .rd_o (d_rd),
     .rs1_o (d_rs1),
@@ -77,10 +82,13 @@ decode #(.DWIDTH(DWIDTH), .AWIDTH(AWIDTH)) decode_0 (
     .imm_o (d_imm)
 );
 
-//logic [DWIDTH - 1:0] f_insn;
-//logic [6:0] d_opcode;
-//logic [6:0] d_funct7;
-//logic [2:0] d_funct3;
+igen #(.DWIDTH(DWIDTH)) igen1 (
+    .opcode_i (d_opcode),
+    .insn_i (f_insn),
+	
+    .imm_o (d_imm)
+);
+
 logic pcsel_out;
 logic immsel_out;
 logic regwren_out;
@@ -91,7 +99,7 @@ logic memwren_out;
 logic [1:0] wbsel_out;
 logic [3:0] alusel_out;
 
-control #(.DWIDTH(DWIDTH)) control_0 (
+control #(.DWIDTH(DWIDTH)) control1 (
     .insn_i (f_insn),
     .opcode_i (d_opcode),
     .funct7_i (d_funct7),
@@ -106,34 +114,6 @@ control #(.DWIDTH(DWIDTH)) control_0 (
     .memwren_o (memwren_out),
     .wbsel_o (wbsel_out),
     .alusel_o (alusel_out)
-);
-
-
-
-/**decode #(.DWIDTH(DWIDTH), .AWIDTH(AWIDTH)) decode_0 (
-
-  	.clk (clk),
-  	.rst (reset),
-	.insn_i (insn_in),
-	.pc_i (pc_in),
-
-    .pc_o (pc_out),
-    .insn_o (insn_out),
-    .opcode_o (opcode_out),
-    .rd_o (rd_out),
-    .rs1_o (rs1_out),
-    .rs2_o (rs2_out),
-    .funct7_o (funct7_out),
-    .funct3_o (funct3_out),
-    .shamt_o (shamt_out),
-    .imm_o (imm_out)
-);
-**/
-logic [DWIDTH-1:0] imm_out_1;
-igen igen_0 (
-    .opcode_i (d_opcode),
-
-    .imm_o (imm_out_1)
 );
 
  /*
