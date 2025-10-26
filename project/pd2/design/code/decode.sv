522707f2603cca4d129059185ff3b0f2134d47ca
Yousif Kndkji <yousif.kndkji@gmail.com>
2025-10-25 23:37:10 -0400

    fix decode.sv formatting mistake

--- project/pd2/design/code/decode.sv
+++ project/pd2/design/code/decode.sv
@@ -28,25 +28,25 @@
 module decode #(
     parameter int DWIDTH=32,
     parameter int AWIDTH=32
-	)(
-		// inputs
-		input logic clk,
-		input logic rst,
-		input logic [DWIDTH - 1:0] insn_i,
-		input logic [DWIDTH - 1:0] pc_i,
+) (
+	// inputs
+	input logic clk,
+	input logic rst,
+	input logic [DWIDTH - 1:0] insn_i,
+	input logic [DWIDTH - 1:0] pc_i,
 
-		// outputs
-		output logic [AWIDTH-1:0] pc_o,
-		output logic [DWIDTH-1:0] insn_o,
-		output logic [6:0] opcode_o,
-		output logic [4:0] rd_o,
-		output logic [4:0] rs1_o,
-		output logic [4:0] rs2_o,
-		output logic [6:0] funct7_o,
-		output logic [2:0] funct3_o,
-		output logic [4:0] shamt_o,
-		output logic [DWIDTH-1:0] imm_o
-	);
+	// outputs
+	output logic [AWIDTH-1:0] pc_o,
+	output logic [DWIDTH-1:0] insn_o,
+	output logic [6:0] opcode_o,
+	output logic [4:0] rd_o,
+	output logic [4:0] rs1_o,
+	output logic [4:0] rs2_o,
+	output logic [6:0] funct7_o,
+	output logic [2:0] funct3_o,
+	output logic [4:0] shamt_o,
+	output logic [DWIDTH-1:0] imm_o
+);
 
 	assign pc_o = pc_i;
 	assign insn_o = insn_i;
